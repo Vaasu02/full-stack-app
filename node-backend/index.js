0ae91ff3dc7d6046472fe6c6b80e3f416c6c8512
root <root@DESKTOP-ND6CSH7.localdomain>
2025-11-20 22:56:25 +0530

    chore: Manual CI/CD trigger to test pipeline health

--- node-backend/index.js
+++ node-backend/index.js
@@ -21,6 +21,6 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get('/', (req, res) => {
     res.send('Backend is working!');
 });
-
+// CI/CD Test: New build trigger 1
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
